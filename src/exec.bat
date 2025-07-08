4ca731a0359dceb4aa3d897000ee3913b6fe51c2
neige68 <neige@neige.nu>
2025-07-08 21:17:50 +0900

    feat: リポジトリルートから build, exec を実行する
    CMakeLists.txt をリポジトリルートにも作り、src/ のものは
    add_subdirectory(src) で呼び出す。
    
    build.bat, exec.bat もリポジトリルートに移す。src/ の build.bat,
    exec.bat はリポジトリルートのものを呼び出す。

--- src/exec.bat
+++ src/exec.bat
@@ -11,50 +11,6 @@ rem
 
 setlocal
 pushd %~dp0
-set @config=debug
-set @name=LegacyPrintDlg
-set @verbose=
-:loop
-if "%1"=="--" goto optbreak
-if "%1"=="r" goto rel
-if "%1"=="-r" goto rel
-if "%1"=="rel" goto rel
-if "%1"=="d" goto deb
-if "%1"=="-d" goto deb
-if "%1"=="deb" goto deb
-if "%1"=="v" goto verbose
-if "%1"=="-v" goto verbose
-if "%1"=="h" goto help
-if "%1"=="-h" goto help
-if "%1"=="--help" goto help
-goto optend
-:rel
-set @config=release
-shift
-goto loop
-:deb
-set @config=debug
-shift
-goto loop
-:verbose
-set @verbose=t
-shift
-goto loop
-:optbreak
-shift
-:optend
-if not "%1"=="" set @name=%1
-if "%@name%"=="" goto help
-set @exe=%@name%.exe
-if not "%@verbose%"=="" echo INFO: %@exe%
-shift
-shift
-pushd ..\build\%@config%
-%@exe% %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
-popd
-goto end
-:help
-echo usage: exec [v] [deb/rel] [--] name
-:end
-popd
+call ..\exec.bat %*
+
 rem end of exec.bat
