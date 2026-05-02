5d94feaa374046133583047012ce88368506cc13
neige68 <neige@neige.nu>
2026-05-02 20:59:25 +0900

    refactor: OWLNext フレームワーク除去

--- src/pch.h
+++ src/pch.h
@@ -1,4 +1,4 @@
-// <pch.h>
+﻿// <pch.h>
 //
 // Project LegacyPrintDlg
 // Copyright (C) 2025 neige68
@@ -9,15 +9,17 @@
 /// \brief Precompile header
 //
 // Compiler: VC14.3
-// Library:  OWL7
 //
 
 #ifndef pch_h
 #define pch_h
 
-//#define _OWLALLPCH
-#define _OWLPCH
-#include <owl/pch.h>
+#define STRICT
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <commctrl.h>
+#include <shellapi.h>
+#include <tchar.h>
 
 #endif // pch_h
 
